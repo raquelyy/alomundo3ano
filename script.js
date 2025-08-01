938a36bcb26ee4d05a7384bde49d507c30f65992
raquelyy <164918770+raquelyy@users.noreply.github.com>
2025-08-01 15:13:00 -0300

    materia

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function inserirNome (){
     console.log(elementos);
     elementos.textContent = nomeUsuario;
 }
+
+const lista = document.querySelector('#lista');
+
+lista.textContent = linguagens[0];
+lista.textContent = linguagens[1];
+lista.textContent = linguagem[2];
+
+let aluno1 = {
+    nome: 'Raquely'
+    Idade: 17,
+    anoLetivo: 'Cursando o ensino médio',
+    materiasFavoritas:['Matematica II','biologia','educação física']
+}
